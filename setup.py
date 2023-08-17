f55dca414fb8ef65b71c219776dc7f01dbb35776
Fatih Mehmet ARSLAN <fatiharslan@modulairy.com>
2023-08-17 12:20:45 +0300

    name changed

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="mail-sender",
+    name="ma-mail-sender",
     version="0.0.1",
     author="Fatih Mehmet ARSLAN",
     author_email="contact@fmarslan.com",
     description="This script enables sending emails asynchronously from Azure Service Bus. Each bus message contains a sender's email address along with SMTP configurations. The script utilizes its SMTP configuration of the message for sending emails.",
-    long_description=open("Readme.md").read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     platforms="all",
     classifiers=[
@@ -26,6 +26,6 @@ setuptools.setup(
     ],
     install_requires=["aiosmtplib","azure-servicebus"],
     python_requires=">=3.8, <4",
-    packages=['mail_sender'],
-    scripts=['bin/mail-sender']
+    packages=['ma_mail_sender'],
+    scripts=['bin/ma-mail-sender']
 )
